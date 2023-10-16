19e296f675a6af905dbf600d60e520925fb06e0f
Dan Snow <des00012@mix.wvu.edu>
2023-10-16 12:48:30 -0400

    Hint Box
    Added Hint box, changed overall Font to something reasonable

---
+++ Website.js
@@ -0,0 +1,12 @@
+var hintsText = ["To engage in combat, move into the enemy", "Gather items to help fight off enemys", "If you hit the dragon, it might not like you", "Did you know that 2 to the 3rd power is 8?", "If you look around, there might be a secret room or two somewhere...", "Watch out for goblins! Those Pesky little Buggers will neevr leave you alone",
+ "The minotaur is a scary beast, I wonder if it's within the maze?", "The cake is a lie"];
+var counter = 0;
+var inst = setInterval(changeHints, 5000);
+function changeHints() {
+  elem.textContent = hintsText[counter];
+  counter++;
+  if (counter >= hintsText.length) {
+    counter = 0;
+    clearInterval(inst); 
+  }
+}
