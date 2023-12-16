81762898c65f22bc3ef6c79dc9a4ad91abf40493
cyblair <cy@theblairs.us>
2023-12-16 05:14:12 -0500

    Serialization for PriorityList

--- PriorityList.js
+++ PriorityList.js
@@ -23,6 +23,21 @@ class Action
     resetPriority(){
         this.priority = this.base;
     }
+
+    serialize()
+    {
+        return JSON.stringify({
+            name : this.name,
+            priority : this.priority,
+            base : this.base,
+            increment : this.increment
+        });
+    }
+
+    static deserialize(obj)
+    {
+        return new Action(obj.name, obj.priority, obj.base, obj.increment);
+    }
 }
 
 class PriorityList
@@ -59,5 +74,29 @@ class PriorityList
     getPriorityAction()
     {
         return this.AIDict[0];
+    }
+
+    serialize()
+    {
+        let str = '[';
+        for (let i = 0; i < this.AIDict.length; i++)
+        {
+            const action = this.AIDict[i];
+            str += c.serialize();
+            if (i != this.AIDict.length-1)
+                str += ', ';
+        }
+        str += ']';
+        return str;
+    }
+
+    static deserialize(obj)
+    {
+        let actions = [];
+        for (const action of obj)
+        {
+            actions.push(Action.deserialize(action));
+        }
+        return new PriorityList(actions);
     }
 }
