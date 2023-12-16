825ee0f8e7f4b516b8c1cf0e4c4f3c2c353dd7f2
cyblair <cy@theblairs.us>
2023-12-16 03:28:58 -0500

    Update EnumeratedValue.js
    Added some documentation and removed console output.

--- EnumeratedValue.js
+++ EnumeratedValue.js
@@ -1,3 +1,6 @@
+/**
+ *  EnumeratedValue class. Wraps a value and array of valid values.
+ */
 export default class EnumeratedValue
 {
     values;
@@ -5,7 +8,7 @@ export default class EnumeratedValue
     updateFunction;
     owner;
 
-    constructor(values, valueUpdatedFunc, owner)
+    constructor(values, valueUpdatedFunc=null, owner=null)
     {
         if (values.length <= 0) {
             throw new Error("EnumeratedValue: 'values' argument must contain at minimum 1 value.");
@@ -15,12 +18,26 @@ export default class EnumeratedValue
         this.values = values;
         this.selected = this.values[0];
         this.updateFunction = valueUpdatedFunc;
-        console.log(this.updateFunction + " : " + valueUpdatedFunc);
     }
 
+    /**
+     * value setter, passes changes to the EnumeratedValue up to it's owner's updateFunction
+     * so you can define how an object that has this as a member behaves when this value
+     * changes.
+     */
     set value(value)
     {
         this.selected = value;
-        this.updateFunction.call(this.owner, value);
+
+        if (this.owner != null)
+            this.updateFunction.call(this.owner, value);
+    }
+
+    setValue(value)
+    {
+        if (this.values.find((e) => e == value) == undefined)
+            throw new Error('Cannot set value of EnumeratedValue to invalid value.');
+
+        this.value = value;
     }
 }
